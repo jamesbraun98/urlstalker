5dd048513d46c886a6ba8c8d14d7bb870f68cf56
james.braun <james.braun@gd-ais.com>
2011-09-23 18:12:00 +0100

    Added spec for gist lib

---
+++ spec/lib/gist_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Gist do
+  before do
+    @gist_id = "12345"
+    gist_response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<gists type=\"array\">\n <gist>\n <repo>#{@gist_id}</repo>\n  </gist>\n</gists>\n"
+    @gist_client =  mock(
+        :post => gist_response,
+        :put => gist_response,
+        :get => "data1"
+      )
+
+    RestClient::Resource.stub!(:new).and_return(@gist_client)
+    HTTParty.stub!(:get).and_return( {"gists" => [{"files" => ["test1.html"]}]} )
+  end
+
+  context "with no id" do
+
+    it "should gets the gist id from the response" do
+      test_gist = Gist.new('test1', 'data1')
+      gist_id = test_gist.publish
+      gist_id.should == @gist_id
+    end
+
+    it "should call the post command" do
+      @gist_client.should_receive(:post)
+      test_gist = Gist.new('test1', 'data1')
+      gist_id = test_gist.publish
+    end
+
+  end
+
+  context "with an id" do
+    it "should call put if the data is different" do
+      @gist_client.should_receive(:put)
+      test_gist = Gist.new('test1', 'data2', @gist_id)
+      gist_id = test_gist.publish
+
+    end
+
+     it "should not call put if the data is the same" do
+      @gist_client.should_not_receive(:put)
+      test_gist = Gist.new('test1', 'data1',  @gist_id)
+      gist_id = test_gist.publish
+
+    end
+
+  end
+
+end
+
