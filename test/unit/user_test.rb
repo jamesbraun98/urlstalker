6372ec3de172176de64bafc2905f0c5b9d36486c
james.braun <james.braun@gd-ais.com>
2011-09-23 13:20:30 +0100

    Fixed failing user test by adding required fields

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   should "be valid" do
-    assert User.new.valid?
+    assert User.new(:email=>'johnDoe@fake.com', :password=>'p@ssw0rd!').valid?
   end
 end
